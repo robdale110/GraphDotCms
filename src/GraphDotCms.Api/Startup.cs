8749e9134f3c2a46a9c79e012e5ed148205f2a4c
Rob Dale <rob@robdale.me.uk>
2020-04-20 15:54:58 +0100

    Added serilog logging to log to the console and to seq

--- src/GraphDotCms.Api/Startup.cs
+++ src/GraphDotCms.Api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace GraphDotCms.Api
 {
@@ -25,6 +26,7 @@ namespace GraphDotCms.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSerilogRequestLogging();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
